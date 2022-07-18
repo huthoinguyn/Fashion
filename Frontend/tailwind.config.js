9d3432911ef93479d4f89d9ed53dfd2495a3ac9f
huthoinguyn <huthoi99@gmail.com>
2022-07-19 01:33:18 +0700

    Add Header & Form & Loading

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Montserrat' : ['Montserrat', 'sans-serif']
+      },
+      colors: {
+        'pri' : '#e65540'
+      }
+    },
+  },
+  plugins: [],
+};
